a0da9ea3ff16c76ec65eb0867a6595aacfc27cef
Dwaipayan <dwaipayandutt@gmail.com>
2025-04-01 15:57:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -80,4 +80,4 @@ async def predict_survival(passenger: PassengerRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    uvicorn.run(app, host="127.0.0.1", port=8001)
