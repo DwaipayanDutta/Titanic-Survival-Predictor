8a1fac20c9a1d4fd61939151f58d531ba35e5430
Dwaipayan <dwaipayandutt@gmail.com>
2025-04-01 15:11:26 +0530

    Updated

---
+++ Model.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+import joblib
+
+# Load data
+url = "Data/titanic.csv"
+df = pd.read_csv(url)
+
+def preprocess_data(df):
+    df = df[['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked', 'Survived']].copy()
+    df.loc[:, 'Embarked'] = df['Embarked'].fillna(df['Embarked'].mode()[0])
+    return df.dropna(subset=['Survived'])
+
+df = preprocess_data(df)
+X = df.drop('Survived', axis=1)
+y = df['Survived']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+numeric_features = ['Age', 'Fare']
+numeric_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='median')),
+    ('scaler', StandardScaler())])
+
+categorical_features = ['Sex', 'Embarked', 'Pclass']
+categorical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='most_frequent')),
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))])
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numeric_transformer, numeric_features),
+        ('cat', categorical_transformer, categorical_features)])
+
+model = Pipeline(steps=[
+    ('preprocessor', preprocessor),
+    ('classifier', LogisticRegression(solver='lbfgs', max_iter=1000))
+])
+model.fit(X_train, y_train)
+
+joblib.dump(model, 'titanic_model.pkl
+print(f"Train Accuracy: {model.score(X_train, y_train):.2f}")
+print(f"Test Accuracy: {model.score(X_test, y_test):.2f}")
+
